00a41ecd8c0b9102d61809462c7bb2c6575c61f1
Surya SIngh <144656857+EndlessGoodness@users.noreply.github.com>
2025-06-15 19:44:41 +0530

    Added Form Validation

--- newbook.js
+++ newbook.js
@@ -10,9 +10,27 @@ function submitForm() {
     const edition = document.getElementById('edition').value;
     const read = document.getElementById('read').checked;
 
+   const inputs=document.getElementById('add-book').getElementsByTagName('input');
+    let isvalid=true;
+    for(let i=0;i<inputs.length;i++){
+        if(!inputs[i].checkValidity()){
+            isvalid=false;
+            document.getElementById("demo").innerHTML="Input required";
+            break;
+        }
+    }
+    if(isvalid){
+        addBookToLibrary(title, author, edition, read);
+        loopthroughLibrary();
+
+        document.getElementById('add-book').reset();
+        document.getElementById('add-book').style.display = 'none';
+        document.getElementById('demo').style.display = 'none';
+    }
+}/*
     addBookToLibrary(title, author, edition, read);
     loopthroughLibrary();
 
     document.getElementById('add-book').reset();
     document.getElementById('add-book').style.display = 'none';
-}
+}*/
